cdc923bf7530dbd5fccdefc2c762cd6915824c11
Oliver Klee <typo3-coding@oliverklee.de>
2026-04-30 17:45:51 +0200

    [BUGFIX] Make the date of birth field unsigned
    Having a negative date of birth (timestamp) does not make sense.

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE fe_users
 (
 	full_salutation            varchar(255) DEFAULT '' NOT NULL,
 	gender                     tinyint(2) unsigned DEFAULT 99 NOT NULL,
-	date_of_birth              int(11) DEFAULT 0 NOT NULL,
+	date_of_birth              int(11) unsigned DEFAULT 0 NOT NULL,
 	zone                       varchar(45)  DEFAULT '' NOT NULL,
 	privacy                    tinyint(1) unsigned DEFAULT 0 NOT NULL,
 	privacy_date_of_acceptance int(11) unsigned DEFAULT 0 NOT NULL,
